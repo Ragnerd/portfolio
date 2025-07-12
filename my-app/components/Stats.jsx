d0bf685ac929de553f7733607ba2438803cc5c39
Ragnerd <foysalakondo97@gmail.com>
2025-07-12 22:17:40 +0600

    updating stats

--- my-app/components/Stats.jsx
+++ my-app/components/Stats.jsx
@@ -1,8 +1,41 @@
 "use client";
 import CountUp from "react-countup";
 
+const stats = [
+  {
+    num: "2",
+    text: "Years of experience",
+  },
+  {
+    num: 7,
+    text: "Projects completed",
+  },
+  {
+    num: 5,
+    text: "Technologies mastered",
+  },
+  {
+    num: 500,
+    text: "Code commits",
+  },
+];
+
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div>
+          {stats.map((item, index) => {
+            return (
+              <div key={index}>
+                <CountUp end={item.num} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Stats;
