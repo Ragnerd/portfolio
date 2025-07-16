80bbe06207b49fd127a3c4acbc2c62e1e6e8d291
Ragnerd <foysalakondo97@gmail.com>
2025-07-17 03:42:02 +0600

    resume and service page added

--- my-app/app/resume/page.jsx
+++ my-app/app/resume/page.jsx
@@ -1,5 +1,149 @@
+"use client";
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+//about data
+const about = {
+  title: "About Me",
+  description:
+    "I am a passionate web developer with a focus on creating dynamic and responsive web applications. My expertise lies in both front-end and back-end technologies, allowing me to build complete solutions that meet user needs.",
+  info: [
+    { Name: "Name", Value: "Moinul Islam Faisal" },
+    { Name: "Phone", Value: "(+880) 1402-002561, (+880)1521-224726" },
+    { Name: "Experience", Value: "Faisal" },
+    { Name: "Vercel", Value: "https://vercel.com/ragnerds-projects" },
+    { Name: "Nationality", Value: "Bangladeshi" },
+    { Name: "Email", Value: "moinulislamfoysal.ie@gmail.com" },
+    { Name: "Freelance", Value: "Available" },
+    { Name: "Language", Value: "English(Fluent), Bangla" },
+  ],
+};
+
+//expertise data
+const experience = {
+  icon: "my-app/assets/medal.png",
+  title: "My Experience",
+  description:
+    "I am a passionate web developer with a focus on creating dynamic and responsive web applications. My expertise lies in both front-end and back-end technologies, allowing me to build complete solutions that meet user needs.",
+  items: [
+    {
+      company: "Tech Solutions Inc",
+      position: "Full Stack Developer",
+      duration: "Jan 2020 - Present",
+    },
+    {
+      company: "Tech Solutions Inc",
+      position: "Senior/Lead Developer",
+      duration: "Jan 2020 - Present",
+    },
+    {
+      company: "Tech Solutions Inc",
+      position: "Mid Level Developer",
+      duration: "Jan 2019 - 2020 ",
+    },
+    {
+      company: "Tech Solutions Inc",
+      position: "Junior Developer and/or Intern",
+      duration: "Jan 2018 - 2019",
+    },
+  ],
+};
+
+//education data
+const education = {
+  icon: "my-app/assets/education.png",
+  title: "My Education",
+  description:
+    "I am a passionate web developer with a focus on creating dynamic and responsive web applications. My expertise lies in both front-end and back-end technologies, allowing me to build complete solutions that meet user needs.",
+  items: [
+    {
+      institution: "Online Course Platforms",
+      degree: "Full Stack Developer",
+      duration: "Jan 2020 - Present",
+    },
+    {
+      institution: "Codecademy",
+      degree: "Senior/Lead Developer",
+      duration: "Jan 2020 - Present",
+    },
+    {
+      institution: "Online Courses",
+      degree: "Mid Level Developer",
+      duration: "Jan 2019 - 2020 ",
+    },
+    {
+      institution: "Tech Institute",
+      degree: "Junior Developer and/or Intern",
+      duration: "Jan 2018 - 2019",
+    },
+    {
+      institution: "Design School",
+      degree: "Junior Developer and/or Intern",
+      duration: "Jan 2018 - 2019",
+    },
+  ],
+};
+
+//skills data
+const skills = {
+  title: "My Skills",
+  description:
+    "I am a passionate web developer with a focus on creating dynamic and responsive web applications. My expertise lies in both front-end and back-end technologies, allowing me to build complete solutions that meet user needs.",
+  skillList: [
+    { skill: "HTML5", icon: <FaHtml5 /> },
+    { skill: "CSS3", icon: <FaCss3 /> },
+    { skill: "JavaScript", icon: <FaJs /> },
+    { skill: "React", icon: <FaReact /> },
+    { skill: "Figma", icon: <FaFigma /> },
+    { skill: "Node.js", icon: <FaNodeJs /> },
+    { skill: "Tailwind CSS", icon: <SiTailwindcss /> },
+    { skill: "Next.js", icon: <SiNextdotjs /> },
+  ],
+};
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "motion/react";
+
 const Resume = () => {
-  return <div>resume page</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About Me</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Resume;
--- my-app/app/services/page.jsx
+++ my-app/app/services/page.jsx
@@ -6,25 +6,29 @@ const services = [
   {
     num: "01",
     title: "Web Development",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    description:
+      "I am a passionate web developer with a focus on creating dynamic and responsive web applications. My expertise lies in both front-end and back-end technologies, allowing me to build complete solutions that meet user needs.",
     href: "",
   },
   {
     num: "02",
     title: "UI/UX design",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    description:
+      "I am a passionate web developer with a focus on creating dynamic and responsive web applications. My expertise lies in both front-end and back-end technologies, allowing me to build complete solutions that meet user needs.",
     href: "",
   },
   {
     num: "03",
     title: "Adobe Suits",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    description:
+      "I am a passionate web developer with a focus on creating dynamic and responsive web applications. My expertise lies in both front-end and back-end technologies, allowing me to build complete solutions that meet user needs.",
     href: "",
   },
   {
     num: "04",
     title: "SEO",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+    description:
+      "I am a passionate web developer with a focus on creating dynamic and responsive web applications. My expertise lies in both front-end and back-end technologies, allowing me to build complete solutions that meet user needs.",
     href: "",
   },
 ];
@@ -33,7 +37,7 @@ import { easeInOut, motion } from "motion/react";
 
 const Services = () => {
   return (
-    <section className="min-[80vh] flex flex-col justify-center py-12 xl:py-0">
+    <section className="min-[80vh] flex flex-col justify-center py-16 xl:py-16">
       <div className="container mx-auto ">
         <motion.div
           initial={{ opacity: 0 }}
@@ -45,20 +49,30 @@ const Services = () => {
         >
           {services.map((service, index) => {
             return (
-              <div key={index} className="flex flex-col justify-center gap-6 group">
+              <div
+                key={index}
+                className="flex-1 flex flex-col justify-center gap-9 group px-6 xl:px-3"
+              >
                 {/* serial number */}
                 <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold">{service.num}</div>
-                  <Link href={service.href}>
-                    <BsArrowDownRight />
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover">
+                    {service.num}
+                  </div>
+                  <Link
+                    href={service.href}
+                    className="w-[50px] h-[50px] rounded-full bg-white hover:bg-accent-default transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl" />
                   </Link>
                 </div>
 
                 {/* title */}
-                <h2>{service.title}</h2>
+                <h2 className="text-[42px] font-bold leading-none text-white hover:text-accent-default trasition-all duration-500">
+                  {service.title}
+                </h2>
 
                 {/* description */}
-                <p>{service.description}</p>
+                <p className="text-white/85 font-stretch-expanded">{service.description}</p>
                 <div className="border-b border-white/30 w-full"></div>
               </div>
             );
